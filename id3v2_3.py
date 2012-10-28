8c2e7f7d9309564093a80a3335a6531f5e7b6e6b
DimaWittmann <dimawittmann@gmail.com>
2012-10-28 20:05:38 +0300

    Додана підтримка всіх форматів кодування інфи

--- id3v2_3.py
+++ id3v2_3.py
@@ -1,7 +1,13 @@
-#Модуль розбору айдіофайлу з кодуванням id3v2.3 і id3v2.4
 
+"""
+Модуль розбору айдіофайлу з кодуванням id3v2.3
+"""
 #Словник, що зберігатиме знайдені теги
-frameList={}
+__frameList={}
+#список, що зберігає назви потрібних вреймів
+__listOfDefFrame= [ "TALB", "TIT1", "TIT2", "TOPE",
+                   "TRCK", "TYER", "TPE1", "TOFN"]
+
 
 def readChar(file):
     c=file.read(1)
@@ -13,14 +19,18 @@ def readByte(file):
     c=ord(c)
     return c
 
-def readString(file,size):
+def readString(file,size):        
     s=""
     for i in range(size):
         s+=readChar(file)
     return s
 
-#фія розбору розміру тегу
+ 
 def compileSize(file):
+    """
+    Розбір байтів з розмрім фрейма,
+    кожеy старший біт байта дорівнює нулю
+    """
     res=0;
     for i in range(3):
         tmp=readByte(file)
@@ -44,18 +54,26 @@ def parsHeader(file):
         head["size"]=int(compileSize(file))
     return head
 
-def parsFile(fileName):
+def parsFile(fileName,requiredFrames=__listOfDefFrame):
+    """
+    Виконує розбір файла від початку до кінця
+    і повертає словник з фреймами, назви яких
+    описані в RequiredFrames 
+    """
     passed = 0  # ксть розібраних байтів
     with open(fileName,"rb") as f:
         header = parsHeader(f)
         while( passed<header["size"] ):
-            passed += parsFrame(f)    
-    info=compileRecordInfo(f)
-    print (header)
-    print (info)
+            passed += parsFrame(f)
+
+    info=compileRecordInfo(f,requiredFrames)
+    return info
 
 
-def parsFrame(file):
+def parsFrame(file,encoding="Latin-1"):
+    """
+    Розбір наступного фрейму в файлі
+    """
     name=readString(file,4)
     frame = {"id":"","size":0,"flags":0,"text":""}
     frame["id"]=name
@@ -64,20 +82,41 @@ def parsFrame(file):
     flag=readByte(file)
     flag=flag*(2**8)            #зсув на 8
     flag=flag+readByte(file)
-    text=readString(file, size)
+    text=file.read(size)
     frame["text"]=text
-    frameList[name]=frame
+    __frameList[name]=frame
     return size
 
-def compileRecordInfo(file):
-    listOfMust = [ "TALB", "TIT1", "TIT2", "TOPE",
-                   "TRCK", "TYER", "TPE1", "TOFN"]
-    dictOfMust = {}
-    for i in range(len(listOfMust)):
-        if (frameList.get(listOfMust[i])!=None):
-            dictOfMust[listOfMust[i]] = frameList[listOfMust[i]]["text"]
-    return dictOfMust
-        
-parsFile ("NC 2 U.mp3")
+
+def decodeFrameInfo(dictOfFrames,encoding="Windows-1251"):
+    """
+    Декодувати текстові поля відповідним кодування Latin-1(windows-1251) чи utf-16 
+    """
+    decodedInfo={}
+    for frame in dictOfFrames:
+        if dictOfFrames[frame][0]==0:
+            decodedInfo[frame] = bytes.decode( dictOfFrames[frame][1:],encoding)                
+        elif dictOfFrames[frame][0]==1:
+            decodedInfo[frame] = bytes.decode( dictOfFrames[frame][1:],encoding="utf-16")
+        elif dictOfFrames[frame][0]==2:
+            decodedInfo[frame] = bytes.decode( dictOfFrames[frame][1:],encoding="utf-16")
+        elif dictOfFrames[frame][0]==3:
+            decodedInfo[frame] = bytes.decode( dictOfFrames[frame][1:],encoding="utf-8")
+    return decodedInfo
+                
+
+def compileRecordInfo(file,listOfRequir):
+    """
+    Збираєму з всіх розібраних фреймів потрібні для нас інформацію
+    Отримуємо словник frameId : value
+    """
+    dictOfRequir = {}
+    for i in range(len(listOfRequir)):
+        if (__frameList.get(listOfRequir[i])!=None):
+            dictOfRequir[listOfRequir[i]] = __frameList[listOfRequir[i]]["text"]
+    return dictOfRequir
+
+       
+info=parsFile ("NC 2 U.mp3")
 
 
